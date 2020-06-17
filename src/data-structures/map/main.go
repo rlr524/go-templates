8a5a7138b1090e63d5bdcc3cd256e36c462e5f47
Rob Ranf <robranf@gmail.com>
2020-06-17 13:52:37 -0700

    fixing slice and structs

--- src/data-structures/map/main.go
+++ src/data-structures/map/main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"html/template"
+	"log"
+	"os"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
+}
+
 func main() {
-	$END$
+	sages := map[string]string{
+		"India": "Ghandi",
+		"America": "MLK",
+		"Meditate": "Buddah",
+		"Love": "Jesus",
+		"Prophet": "Muhammad",
+		"The Way": "Miyamoto Musashi",
+		"There is no try": "Yoda",
+	}
+
+	// here we're passing in the map named sages as the data
+	err := tpl.Execute(os.Stdout, sages)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
